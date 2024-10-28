1dae851d56b30b7447d8789a145291d3c0d849c4
anushkakawale <anushkakawale04102001@gmail.com>
2024-10-28 14:48:58 +0530

    Time: 2 ms (66.31%), Space: 12.2 MB (92.35%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_map:
+                return [num_map[diff], i]
+            num_map[num] = i
+        
